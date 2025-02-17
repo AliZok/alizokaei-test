19f8cdad468997605011a6b3cf008227d2e36f94
ali <ali.zokaei.1367@gmail.com>
2025-02-18 01:31:47 +0330

    init project vite-vue-tailwind

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="text-[30px]">
+    <!-- Using utilities -->
+<button class="py-2 px-4 font-semibold rounded-lg shadow-md text-white dark:bg-blue-500 bg-green-100 hover:bg-green-700">
+  Click me
+</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
