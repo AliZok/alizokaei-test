67a7877e31da4da367b248368b9c9ed21896f984
ali <ali.zokaei.1367@gmail.com>
2025-02-18 23:47:09 +0330

    fix search

--- src/components/Faq2.vue
+++ src/components/Faq2.vue
@@ -9,6 +9,9 @@
         <div class="w-[200px] inline-block">
           <SearchInput v-model="searchPhrase" />
         </div>
+        <div v-if="debouncedSearch">
+          Searching for: {{ debouncedSearch }}
+        </div>
       </div>
       <div>
         <transition-group name="list" tag="div">
@@ -31,7 +34,8 @@
 </template>
 
 <script setup>
-import { computed, onMounted, ref } from 'vue'
+
+import { computed, onMounted, ref, watch } from 'vue'
 import { useFunctions } from '@/composables/useFunctions.js'
 import { useApi } from '@/composables/useApi.js'
 import FAQCategory from '@/components/FAQ/FAQCategory.vue'
@@ -60,6 +64,7 @@ const getFaqList = () => {
 
   getData(path, params).then((responce) => {
     faqList.value = responce.result
+    pureList.value = faqList.value
     groupedByCategory()
   })
 }
@@ -79,6 +84,48 @@ const groupedByCategory = () => {
 
 
 getFaqList()
+
+
+const debouncedSearch = ref('');
+const debounceTimeout = ref(null);
+
+watch(searchPhrase, (newValue) => {
+  // if (newValue.length > 2) {
+  if (debounceTimeout.value) {
+    clearTimeout(debounceTimeout.value);
+  }
+
+  debounceTimeout.value = setTimeout(() => {
+    debouncedSearch.value = newValue;
+  }, 800);
+  // }
+});
+
+const isSeraching = ref(false)
+const pureList = ref([])
+
+watch(debouncedSearch, (newValue) => {
+  search(newValue)
+})
+
+const search = (inputPhrase) => {
+  if (inputPhrase) {
+    const lowerInputPhrase = inputPhrase.toLowerCase()
+    faqList.value = faqList.value.filter(item => {
+      return (
+        item.question.toLowerCase().includes(lowerInputPhrase) ||
+        item.category.toLowerCase().includes(lowerInputPhrase) ||
+        item.subcategory.toLowerCase().includes(lowerInputPhrase) ||
+        item.answer.toLowerCase().includes(lowerInputPhrase)
+      );
+    })
+    groupedByCategory()
+  }else{
+    faqList.value = pureList.value
+    groupedByCategory()
+  }
+
+}
 
 onMounted(() => {
 })
--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="max-w-md w-full">
         <div class="relative">
-            <input type="text" v-model="model" @input="onSearch" @focus="isFocused = true"
-                @blur="isFocused = false" placeholder=""
+            <input type="text" v-model="model" @input="onSearch" @focus="isFocused = true" @blur="isFocused = false"
+                placeholder=""
                 class="text-gray-500 w-full pl-10 pr-4 py-[6px] rounded-full off-shadow focus:outline-none  focus:border-transparent transition duration-200 ease-in-out" />
             <div class="absolute inset-y-0 left-0 top-[1px] pl-3 flex items-center pointer-events-none">
                 <i class="fa fa-search h-5 w-5  " :class="isFocused ? 'text-gray-600' : 'text-gray-300'"></i>
@@ -12,13 +12,17 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 
 const model = defineModel()
+
 const isFocused = ref(false)
 const onSearch = () => {
     console.log('Searching for:', model.value);
 };
+
+
+
 </script>
 
 <style scoped>
