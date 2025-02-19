62b595a2dc86f150488480573bd10fecaa94f1f7
Ali Zokaei <ali.zokaei.arnitex@gmail.com>
2025-02-19 10:23:33 +0330

    save

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,9 @@
 // tailwind.config.js
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  // purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: [  './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',],
+  
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
